91a537bd62da9ed856476aab6b8abebf0a0cc9af
Ashwin Chikkayya <ashwin.chikkayya@ness.com>
2021-03-28 02:03:24 +0530

    Updated header and child theme styles.

--- functions.php
+++ functions.php
@@ -11,4 +11,5 @@ add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles', 11 );
  */
 function my_theme_enqueue_styles() {
 	wp_enqueue_style( 'child-style', get_stylesheet_uri(), array(), '0.0.1' );
+	wp_enqueue_style( 'ddp-stle', get_stylesheet_directory_uri() . '/css/ddp-style.css', array(), '0.0.1' );
 }
--- header.php
+++ header.php
@@ -1,9 +1,29 @@
+<?php
+$blog_info    = get_bloginfo( 'name' );
+$description  = get_bloginfo( 'description', 'display' );
+$show_title   = ( true === get_theme_mod( 'display_title_and_tagline', true ) );
+$header_class = $show_title ? 'site-title' : 'screen-reader-text';
+?>
 <!doctype html>
 <html <?php language_attributes(); ?> <?php twentytwentyone_the_html_classes(); ?>>
 <head>
-    <meta charset="<?php bloginfo( 'charset' ); ?>" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <?php wp_head(); ?>
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
+<div id="ddp-header">
+	<div id="ddp-logo">
+		<?php if ( has_custom_logo() ) : ?>
+			<div class="site-logo"><?php the_custom_logo(); ?></div>
+		<?php endif; ?>
+		<?php if ( $show_title ) : ?>
+			<h3><?php echo $blog_info; ?></h3>
+			<p><?php echo $description; ?></p>
+		<?php endif; ?>
+	</div>
+	<div id="ddp-navigation">
+
+	</div>
+</div>
