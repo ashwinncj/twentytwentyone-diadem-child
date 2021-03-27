0910402b6f196c761d1c33e9dc8e141bad3afec8
Ashwin Chikkayya <ashwin.chikkayya@ness.com>
2021-03-28 02:31:44 +0530

    Updated SASS based stylings.

--- header.php
+++ header.php
@@ -14,16 +14,20 @@ $header_class = $show_title ? 'site-title' : 'screen-reader-text';
 
 <body <?php body_class(); ?>>
 <div id="ddp-header">
-	<div id="ddp-logo">
-		<?php if ( has_custom_logo() ) : ?>
-			<div class="site-logo"><?php the_custom_logo(); ?></div>
-		<?php endif; ?>
-		<?php if ( $show_title ) : ?>
-			<h3><?php echo $blog_info; ?></h3>
-			<p><?php echo $description; ?></p>
-		<?php endif; ?>
+	<div id="ddp-site-branding" class="pure-g">
+		<div id="ddp-logo" class="pure-u-1-2">
+			<?php if ( has_custom_logo() ) : ?>
+				<div class="site-logo"><?php the_custom_logo(); ?></div>
+			<?php endif; ?>
+			<?php if ( $show_title ) : ?>
+				<h3><?php echo $blog_info; ?></h3>
+				<p><?php echo $description; ?></p>
+			<?php endif; ?>
+		</div>
+		<div id="ddp-header-subscribe" class="pure-u-1-2">
+		</div>
 	</div>
-	<div id="ddp-navigation">
+	<div id="ddp-header-sticky-nav" class="pure-g">
 
 	</div>
 </div>
